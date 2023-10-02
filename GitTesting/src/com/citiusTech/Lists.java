cd7d42c864182a214f70ff9c380daec3ad33b173
priyajjadhav <piyuvj98@gmail.com>
2023-10-02 15:00:53 +0530

    Lists class added with ArrayList

---
+++ GitTesting/src/com/citiusTech/Lists.java
@@ -0,0 +1,15 @@
+package com.citiusTech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+	public static void main(String[] args) {
+		
+	List al = new ArrayList();
+	al.add(10);
+	al.add(20);
+	al.add(30);
+	al.add(40);
+}
+}
